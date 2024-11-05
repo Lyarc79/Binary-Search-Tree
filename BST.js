3d07c0415a08a7fd69b9e855ecab31cd2a3a21b1
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2024-11-05 17:51:05 +0100

    Moved prettyPrint inside Tree class

--- BST.js
+++ BST.js
@@ -188,19 +188,21 @@ class Tree{
       })
       this.root = this.buildTree(values);
     }
+
+    prettyPrint(node, prefix = "", isLeft = true){
+      if (node === null) {
+        return;
+      }
+      if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      }
+      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+      if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      }
+    };
 }
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
+
 
 module.exports = {Node, Tree};
